a70b3a1a22d2c6dcf1debdea0498d5e9c1a1f3a9
oscarwergun <127099022+oscarwergun@users.noreply.github.com>
2024-06-09 20:12:52 +0200

    added skills on experiences component

--- components/ExperienceCard.tsx
+++ components/ExperienceCard.tsx
@@ -9,25 +9,25 @@ type Props = {
 
 function ExperienceCard({ experience }: Props) {
   return (
-    <section 
-    className="flex flex-col text-start flex-shrink-0 px-3 mx-3 space-y-4 bg-[#49426c] rounded-xl w-[500px] md:w-[600px] lg:w-[700px] snap-center h-[500px] md:h-[600px] lg:h-[700px] opacity-40 hover:opacity-100 pt-16 cursor-pointer transition-opacity duration-200 overflow-hidden hover:bg-[#49426c]/50 ease-out">
-      <div className="flex flex-col items-center justify-center space-y-3 snap-center">
-        <motion.div
-          initial={{ opacity: 0, y: -100 }}
-          whileInView={{ y: 0, opacity: 1 }}
-          transition={{ duration: 1.5 }}
-        >
+    <motion.section
+    initial={{ opacity: 0, y: -100 }}
+    whileInView={{ y: 0, opacity: 1 }}
+    transition={{ duration: 1.5 }}
+    className='flex flex-col items-center space-y-7 flex-shrink-0 snap-center w-[500px] md:w-[600px] xl:w-[900px] bg-[#1e2c3d] p-10 hover:bg-[#26384f] transition-opacity ease-out h-fit min-h-[720px]'>
           <Image
             src={urlFor(experience?.companyImage).url()}
             alt="experience-logo"
             width="180"
             height="180"
-            className="rounded-[20%] md:rounded-lg lg:w-[200px] lg:h-[200px]"
+            className="rounded-[20%] md:rounded-lg lg:w-[200px] lg:h-[200px] mt-12"
           />
-        </motion.div>
         <div className="px-0 py-6 md:px-6">
           <h4 className="text-2xl ">{experience?.company}</h4>
           <p className="mt-1 text-lg">{experience?.title} </p>
+          <div className="flex justify-center mx-auto my-2 space-x-2">
+            {experience?.technologies.map((technique, i) => technique?.image &&
+              <Image key={i} alt="" src={urlFor(technique?.image).url()} width={40} height={40}  className="h-10"/>)}
+          </div>
           <p className="text-sm uppercase text-[#c6bfbf] my-2">
             {new Date(experience.dateStarted).toDateString()}-{" "}
             {experience?.isCurrentlyWorking
@@ -42,8 +42,7 @@ function ExperienceCard({ experience }: Props) {
             ))}
           </ul>
         </div>
-      </div>
-    </section>
+    </motion.section>
   );
 }
 
